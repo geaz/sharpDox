e068af8f187934262c3041563984e05479ab0c19
Gerrit "Geaz" Gazic <mail@geaz.de>
2014-01-10 16:20:39 +0100

    Performance tweaks #2

--- Libraries/SharpDox.UML/Ressources/Icons.cs
+++ Libraries/SharpDox.UML/Ressources/Icons.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.IO;
-using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Resources;
 
 namespace SharpDox.UML.Ressources
 {
     internal static class Icons
     {
-        private static readonly Object _lockObj = new Object();
-
         public static BitmapImage GetIcon(string entityType, string accessibility)
         {
             BitmapImage bitmap = null;
@@ -46,46 +42,39 @@ namespace SharpDox.UML.Ressources
 
         public static string GetBase64Icon(string entityType, string accessibility)
         {
-            Uri uri = null;
-
+            string ressourceName;
             switch (accessibility.ToLower())
             {
                 case "friend":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Friend.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Friend.png", entityType);
                     break;
                 case "private":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Private.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Private.png", entityType);
                     break;
                 case "protected":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Protected.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Protected.png", entityType);
                     break;
                 case "public":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Public.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Public.png", entityType);
                     break;
                 case "sealed":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Sealed.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Sealed.png", entityType);
                     break;
                 case "internal":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Sealed.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Sealed.png", entityType);
                     break;
                 case "shortcut":
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Shortcut.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Shortcut.png", entityType);
                     break;
                 default:
-                    uri = new Uri(string.Format("pack://application:,,,/SharpDox.UML;component/Ressources/Icons/{0}_Public.png", entityType));
+                    ressourceName = string.Format("SharpDox.UML.Ressources.Icons.Embedded.{0}_Public.png", entityType);
                     break;
             }
-
-            StreamResourceInfo sri = null;
-            lock (_lockObj)
-            {
-                sri = Application.GetResourceStream(uri);
-            }
-
+            
             var base64 = string.Empty;
             using (var memoryStream = new MemoryStream())
             {
-                sri.Stream.CopyTo(memoryStream);
+                typeof(Icons).Assembly.GetManifestResourceStream(ressourceName).CopyTo(memoryStream);
                 var result = memoryStream.ToArray();
                 base64 = Convert.ToBase64String(result);
             }
