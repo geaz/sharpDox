9b3de750cf2f81eddbf1135aadf8d6b8cad48a7d
Gerrit Gazic <mail@geaz.de>
2014-07-05 16:06:02 +0200

    Updated gitignore

---
+++ src/SharpDox.Sdk/Build/IBuildController.cs
@@ -0,0 +1,82 @@
+﻿using SharpDox.Sdk.Config;
+
+namespace SharpDox.Sdk.Build
+{
+    /// <default>
+    ///     <summary>
+    ///     This interface exposes all needed functionalities to
+    ///     parse and build documentation.
+    ///     </summary>
+    /// </default>
+    /// <de>
+    ///     <summary>
+    ///     Dieses Interface bildet alle Funktionen ab, die zum
+    ///     Einlesen und Bauen der Dokumentation benötigt wird.
+    ///     </summary>
+    /// </de>
+    public interface IBuildController
+    {
+        /// <default>
+        ///     <summary>
+        ///     Starts a structure parse for a given c# solution.
+        ///     This method only parses minimal information about namespaces, types
+        ///     and members. Just enough to show a tree of all members as seen in the
+        ///     visibility settings of the sharpDox GUI.
+        ///     </summary>
+        ///     <param name="coreConfigSection">A config file with all necessary parameters</param>
+        ///     <param name="thread">Indicates, if the process should start in an own thread.</param>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Startet das Einlesen der Struktur einer gegebenen C# Lösung.
+        ///     Die Methode liest lediglich minimale Informationen über Namensräume, Typen und Mitglieder.
+        ///     Gerade genug, um einen Baum aufzubauen wie er in den Sichtbarkeitseinstellungen der sharpDox GUI zu sehen ist.
+        ///     </summary>
+        ///     <param name="coreConfigSection">Eine Konfigurationsdatei mit allen notwendigen Einstellungen</param>
+        ///     <param name="thread">Gibt an, ob der Vorgang in einem eigenen Thread gestartet werden soll</param>
+        /// </de>
+        void StartParse(ICoreConfigSection coreConfigSection, bool thread);
+
+        /// <default>
+        ///     <summary>
+        ///     Starts a documentation build for a given c# solution.
+        ///     The method parses the solution and starts all registered exporters.
+        ///     </summary>
+        ///     <param name="coreConfigSection">A config file with all necessary parameters</param>
+        ///     <param name="thread">Indicates, if the process should start in an own thread.</param>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Startet den Bau der Dokumentation einer gegebenen C# Lösung.
+        ///     Die Methode liest die Lösung ein und startet alle registrierten 
+        ///     </summary>
+        ///     <param name="coreConfigSection">Eine Konfigurationsdatei mit allen notwendigen Einstellungen</param>
+        ///     <param name="thread">Gibt an, ob der Vorgang in einem eigenen Thread gestartet werden soll</param>
+        /// </de>
+        void StartBuild(ICoreConfigSection coreConfigSection, bool thread);
+
+        /// <default>
+        ///     <summary>
+        ///     Stops any current running parse or build process.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Stoppt den aktuell laufenden Einlese- oder Bau-Prozess.
+        ///     </summary>
+        /// </de>
+        void Stop();
+
+        /// <default>
+        ///     <summary>
+        ///     Gets the <c>BuildMessenger</c> which outputs all build messages.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Holt den <c>BuildMessenger</c> über den alle Nachrichten des Bauvorgangs ausgegeben werden.
+        ///     </summary>
+        /// </de>
+        IBuildMessenger BuildMessenger { get; }
+    }
+}
---
+++ src/SharpDox.Sdk/Build/IBuildMessenger.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using SharpDox.Model.Repository;
+using SharpDox.Model;
+
+namespace SharpDox.Sdk.Build
+{
+    /// <default>
+    ///     <summary>
+    ///     This interface exposes all events used by the implementation of the IBuildController.
+    ///     </summary>
+    /// </default>
+    /// <de>
+    ///     <summary>
+    ///     Dieses Interface bildet alle Events ab, die von der Implementierung des <c>IBuildController</c>s genutzt werden.
+    ///     </summary>
+    /// </de>
+    public interface IBuildMessenger
+    {
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <c>BuildController</c> sends a message during the build process.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls der <c>BuildController</c> eine Nachricht während des Bauvorgangs sendet.
+        ///     </summary>
+        /// </de>
+        event Action<string> OnBuildMessage;
+
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <c>BuildController</c> sends a message during the current step of the build process.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls der <c>BuildController</c> eine Nachricht während des aktuellen Vorgangs sendet.
+        ///     </summary>
+        /// </de>
+        event Action<string> OnStepMessage;
+
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <c>BuildController</c> sends a progress update for the current build process.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls der <c>BuildController</c> eine Fortschrittsmeldung für den aktuellen Bauvorgang sendet.
+        ///     </summary>
+        /// </de>
+        event Action<int> OnBuildProgress;
+
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <c>BuildController</c> sends a progress update for the current step of the build process.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls der <c>BuildController</c> eine Fortschrittsmeldung für den aktuellen Vorgang sendet.
+        ///     </summary>
+        /// </de>
+        event Action<int> OnStepProgress;
+
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <see cref="IBuildController.StartBuild"/> method of the <see cref="IBuildController"/> has stopped.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls die Methode <see cref="IBuildController.StartBuild"/> des <see cref="IBuildController"/> gestoppt wurde.
+        ///     </summary>
+        /// </de>
+        event Action OnBuildStopped;
+
+        event Action OnBuildFailed;
+
+        /// <default>
+        ///     <summary>
+        ///     Gets fired, if the <see cref="IBuildController.StartParse"/> or <see cref="IBuildController.StartBuild"/> method of the <see cref="IBuildController"/> has finished.
+        ///     The <c>SDProject</c> can be <c>null</c>, if the build wasn't successful.
+        ///     </summary>
+        /// </default>
+        /// <de>
+        ///     <summary>
+        ///     Wird gefeurt, falls die Methode <see cref="IBuildController.StartParse"/> oder <see cref="IBuildController.StartBuild"/> des <see cref="IBuildController"/> beendet wurde.
+        ///     Das <c>SDProject</c> kann <c>null</c> sein, falls der Bauvorgang nicht erfolgreich war.
+        ///     </summary>
+        /// </de>
+        event Action<SDProject> OnBuildCompleted;
+    }
+}
