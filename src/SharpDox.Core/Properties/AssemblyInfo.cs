18e62589cb95fecc08316ced9946f1cfe5b5d4a2
Gerrit Gazic <mail@geaz.de>
2014-11-10 19:17:59 +0100

    Added nuget sdk package creation - start of new version

--- src/SharpDox.Core/Properties/AssemblyInfo.cs
+++ src/SharpDox.Core/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.9.9")]
+[assembly: AssemblyFileVersion("0.9.10")]
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
