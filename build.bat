a8df8ecf463d4fb5ac297ea8e4b5647c45e5b0f6
Gerrit "Geaz" Gazic <mail@geaz.de>
2015-09-02 14:09:19 +0200

    added nuget

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+call nuget.exe restore src
 
+set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 call %msBuildDir%\msbuild "msbuild.config" /p:Configuration=Build
