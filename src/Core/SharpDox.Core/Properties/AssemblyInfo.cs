db94f622e2de991408d83fed8c3b29edece94a38
Gerrit Gazic <mail@geaz.de>
2016-05-15 23:29:32 +0200

    Updated to 1.1.1

--- src/Core/SharpDox.Core/Properties/AssemblyInfo.cs
+++ src/Core/SharpDox.Core/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 
 [assembly: InternalsVisibleTo("SharpDox.Config.Tests")]
