21eee0ae48d5f1f98a6df48cb535df7fbee38a97
Gerrit Gazic <mail@geaz.de>
2016-08-15 21:19:16 +0200

    Update Project.js

--- de/doc/data/Project.js
+++ de/doc/data/Project.js
@@ -4,7 +4,7 @@ var sharpDox = sharpDox || {};
 
 sharpDox.projectData = {
   name: 'sharpDox',
-   homepage: 'http://sharpdox.de',    version: '1.2',    author: 'Gerrit "Geaz" Gazic',      disqusShortName: 'sharpdoxdocen',   showCode: true,
+   homepage: 'http://sharpdox.de',    version: '1.2',    author: 'Gerrit "Geaz" Gazic',      disqusShortName: 'sharpdox',   showCode: true,
     hasLogo: true,
   targetFxs: [
 			".NET Framework 4.6",
